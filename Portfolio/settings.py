c48fc8458ef9a5380477dc37a56900ce6a030d0b
Kunal Yadav <kunalyadav2003@gmail.com>
2022-07-30 01:12:03 +0530

    Updated settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
